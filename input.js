34aafc6679e2882d5d6d7bfafdc0359d95a8da41
Jacqueline <jlee4332@gmail.com>
2021-03-10 16:08:41 -0500

    adding up down left right to input.js

--- input.js
+++ input.js
@@ -2,7 +2,10 @@
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = function() {
+let connect;
+
+const setupInput = function(conn) {
+  connect = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -14,8 +17,20 @@ const setupInput = function() {
 const handleUserInput = (key) => {
   if (key === '\u0003') {
     process.exit();
+  } if (key === 'w') {
+    console.log('Up!');
+    connect.write('Move: up');
+  } if (key === 'd') {
+    console.log('right!')
+    connect.write('Move: right');
+  } if (key === 'a') {
+    console.log('left');
+    connect.write('Move: left');
+  } if (key === 's') {
+    console.log('down');
+    connect.write('Move: down');
   }
-  //setup if key = e, write a comment.
+  //setup if key = w, write a comment up.
   //send move commands/directions
 };
 
--- play.js
+++ play.js
@@ -4,7 +4,8 @@ const setupInput = require('./input');
 console.log('Connecting ...');
 connect();
 
-
+let connection = connect();
+setupInput(connection);
 
 
 
